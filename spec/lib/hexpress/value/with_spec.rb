8b1bb80e9d1177cd24687ef9168dcedd49b94559
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-09-04 11:17:42 -0700

    We want to be able to compose patterns.

--- spec/lib/hexpress/value/with_spec.rb
+++ spec/lib/hexpress/value/with_spec.rb
@@ -5,5 +5,12 @@ describe Hexpress::Value::With do
     it "returns the string given" do
       expect(described_class.new("foo").to_s).to eq("foo")
     end
+
+    it "allows composing of multiple patterns" do
+      pattern1 = Hexpress.new.starting("foo")
+      pattern2 = Hexpress.new.ending("bar")
+      pattern3 = Hexpress.new.including(pattern1).with("1").including(pattern2)
+      expect(pattern3.to_r).to eq(/^foo1bar$/)
+    end
   end
 end
