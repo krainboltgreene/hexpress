b7e915ada029f972d9b208625212050b4d5ff5d1
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-09-04 12:56:03 -0700

    Can't use alias_method on main, so we put it on Object.

--- lib/hexpress/main.rb
+++ lib/hexpress/main.rb
@@ -1,6 +1,8 @@
 require "hexpress"
 
-def hex(&block)
-  Hexpress.new(&block)
+class Object
+  def hex(&block)
+    Hexpress.new(&block)
+  end
+  alias_method :hexp, :hex
 end
-alias_method :hexp, :hex
